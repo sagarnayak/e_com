5530dd131d9f210803e0301002d2e7f5363cafc2
sagar <snkumar.nayak@gmail.com>
2021-08-04 19:05:27 +0530

    code re organise

--- src/model/claims.rs
+++ src/model/claims.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use crate::model::auth_roles_cross_paths::AuthRolesCrossPaths;
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Claims {
